4c4414cfde4a5cfb95aad2d6f7ff9732aa726aa3
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2021-07-20 20:00:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-trvcbs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-trvcbs)
---
+++ index.js
@@ -0,0 +1,44 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// This below object helps us understand how the this gets resolved in arrow functions vs
+// the regular objects
+
+const v = {
+  af: 'Hello',
+  bg: 'World',
+  call: function() {
+    let a = 'Method';
+    console.log('Inside the method function', this);
+    return {
+      val: 'Inner method',
+      callInner: function() {
+        console.log('Inside the inner method function', this);
+      }
+    };
+  },
+  arrow: () => {
+    let a = 'Arrow';
+    console.log('Inside the arrow function', this);
+    return {
+      val: 'Inner arrow func',
+      arrowInner: () => {
+        console.log('Inside the inner arrow function', this);
+      }
+    };
+  },
+  callOuter: function() {
+    let a = 'Arrow';
+    console.log('Inside the arrow function', this);
+    return {
+      val: 'Inner arrow func',
+      arrowInner: () => {
+        console.log('Inside the inner arrow function', this);
+      }
+    };
+  }
+};
